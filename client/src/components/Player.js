fbd012a1c5ac1704e255cfca5fb85272a0984ca3
victor tran <victor.tran@tempestas.io>
2020-03-22 23:04:01 -0400

    add more structure to information

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -4,8 +4,16 @@ const Player = ({ name, searches, id, country }) => {
   return (
     <div className="Player Card">
       <img src={`https://robohash.org/${name}.png`} />
-      <p>{name}</p>
-      <p>{country}</p>
+      <section className="Player-info">
+        <section>
+          <p>Name</p>
+          <p>{name}</p>
+        </section>
+        <section>
+          <p>Country</p>
+          <p>{country}</p>
+        </section>
+      </section>
     </div>
   );
 };
