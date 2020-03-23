4421c141e037da928529943e1515152294b7ff63
victor tran <victor.tran@tempestas.io>
2020-03-22 23:04:16 -0400

    add new className to List

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -3,7 +3,7 @@ import { List, withData } from './';
 import { usePlayers } from '../hooks';
 const Players = props => {
   const [players, setPlayers, makePlayers] = usePlayers(props.data);
-  return <List>{makePlayers()}</List>;
+  return <List className="Players">{makePlayers()}</List>;
 };
 
 export default withData(Players)({ url: 'http://localhost:5000/api/players' });
